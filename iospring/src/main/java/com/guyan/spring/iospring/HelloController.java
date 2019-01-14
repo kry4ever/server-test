cb54f920bed7e09affa730c3c3577281d9c6d778
guyan <guyan.00@bytedance.com>
2019-01-14 14:21:44 +0800

    log4j2 add

--- iospring/src/main/java/com/guyan/spring/iospring/HelloController.java
+++ iospring/src/main/java/com/guyan/spring/iospring/HelloController.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,44 +21,21 @@ public class HelloController {
     @Autowired
     private UserDao dao;
 
+    private Logger logger = LogManager.getLogger("TRACE_ALL");
+
     @RequestMapping("/hello")
     public String sayHello() {
 
+
+        logger.info("receiver hello");
         User user = dao.getUserByIdAnno(2);
-
-
-
-//        String resource = "mybatis-config.xml";
-//        InputStream inputStream = null;
-//        try {
-//            inputStream = Resources.getResourceAsStream(resource);
-//            System.out.println("load res success");
-//            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
-//            SqlSession session = factory.openSession();
-//            System.out.println("open session success");
-////            Connection connection = session.getConnection();
-////            System.out.println("get con success");
-//            //---------------
-//            //User user = session.selectOne("Mapper1.queryFirstUser", 10); //参数一：namespace.id
-//            UserMapper mapper = session.getMapper(UserMapper.class);
-//            User user = mapper.getUserById(10);
-//            System.out.println("user is " + user);
-//            //--------------
-////            connection.close();
-//            session.close();
-//
-//            return user.toString();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
+        logger.info("use hello " + user);
 
         return user.toString();
     }
 
     @RequestMapping("/insert")
-    public void insertOne(){
+    public void insertOne() {
         String resource = "mybatis-config.xml";
         InputStream inputStream = null;
         try {
